8db0ba00e61cceb343402b0484651d743ccfde9a
master <master@SMLAB306PC09_25>
2025-04-29 21:18:37 -0500

    CAMBIOS EN EL APPds

--- semana5/App.xaml.cs
+++ semana5/App.xaml.cs
@@ -2,9 +2,12 @@
 {
     public partial class App : Application
     {
-        public App()
+        public static Repository.PersonaRepository personaRepo {  get; set; }
+        public App(Repository.PersonaRepository personaRepository)
         {
             InitializeComponent();
+            personaRepo = personaRepository;
+
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
