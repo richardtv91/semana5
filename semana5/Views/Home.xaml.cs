03c9636194396558e3569ecdc750d4421d805865
master <master@SMLAB306PC09_25>
2025-04-29 21:37:12 -0500

    camvios en los botones programacion

--- semana5/Views/Home.xaml.cs
+++ semana5/Views/Home.xaml.cs
@@ -1,3 +1,5 @@
+using semana5.Models;
+
 namespace semana5.Views;
 
 public partial class Home : ContentPage
@@ -9,6 +11,14 @@ public partial class Home : ContentPage
 
     private void btnInsertar_Clicked(object sender, EventArgs e)
     {
-
+		statusMessage.Text = "";
+		App.personaRepo.AddNewPersona(txtNombre.Text);
+		statusMessage.Text = App.personaRepo.statusMesage;
     }
+	private void btnListar_Clicked(Object sender, EventArgs e)
+	{
+		statusMessage.Text = "";
+		List<Persona> lista= App.personaRepo.GetAllPersona();
+		listaPersona.ItemsSource = lista;
+	}
 }
